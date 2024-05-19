014bbaeb34cc5901529535732fb21d87002742e9
khalid <K.abdillahi@alustudent.com>
2024-05-19 13:52:03 +0200

    khalo

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-""" Minimum Operations"""
+"""Minimum Operations"""
 
 def minOperations(n):
-    """ Minimum Operations"""
+    """Calculate the minimum number of operations needed to reach n H characters in a text file."""
     if n <= 1:
         return 0
     i = 2
@@ -10,7 +10,7 @@ def minOperations(n):
     while i <= n:
         if n % i == 0:
             result += i
-            n = n / i
+            n /= i
         else:
             i += 1
     return result
